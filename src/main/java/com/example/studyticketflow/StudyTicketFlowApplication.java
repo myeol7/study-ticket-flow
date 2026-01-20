b124a28576d0822e075be74d0839e4fc01ac6546
myeol7 <tytekflzz@naver.com>
2026-01-20 15:51:27 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/example/studyticketflow/StudyTicketFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyticketflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTicketFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTicketFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studyticketflow/StudyTicketFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyticketflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTicketFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
