7faea0bc465ca51492ef91c51dee1fbc91962890
myeol7 <tytekflzz@naver.com>
2026-02-18 13:37:39 +0900

    feat: 비관적 락 적용 및 동시성 테스트 진행

--- src/main/java/com/example/ticketflow/domain/seat/SeatRepository.java
+++ src/main/java/com/example/ticketflow/domain/seat/SeatRepository.java
@@ -1,11 +1,22 @@
 package com.example.ticketflow.domain.seat;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;// DAO와 비슷한 개념
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
 
     // select * from seat where hall_id = ?
     List<Seat> findAllByHallId(Long hallId);
+
+    // 비관적 락을 건 조회 메서드
+    // select * from seat where id = ? for update
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM Seat s WHERE s.id = :id")
+    Optional<Seat> findByIdWithLock(@Param("id") Long id);
 }
