268cfcccb17c1c041932786d58ffe31c1c7bdb93
myeol7 <tytekflzz@naver.com>
2026-03-09 13:41:42 +0900

    feat: 인터셉터를 활용하여 멱등성 방어 로직 구현

---
+++ src/main/java/com/example/ticketflow/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.ticketflow.config;
+
+import com.example.ticketflow.interfaces.interceptor.IdempotencyInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final IdempotencyInterceptor idempotencyInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(idempotencyInterceptor)
+                .addPathPatterns("/api/reservations/**"); // API 경로에만 인터셉터 적용
+    }
+}
