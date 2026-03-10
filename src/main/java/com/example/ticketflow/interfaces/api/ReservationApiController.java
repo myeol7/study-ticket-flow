567a96047517502491f10384e0cfd50021b64d77
myeol7 <tytekflzz@naver.com>
2026-03-10 14:05:06 +0900

    refactor: 트랜잭션 정합성을 위해 멱등성을 서비스 계층으로 이동

--- src/main/java/com/example/ticketflow/interfaces/api/ReservationApiController.java
+++ src/main/java/com/example/ticketflow/interfaces/api/ReservationApiController.java
@@ -3,11 +3,9 @@ package com.example.ticketflow.interfaces.api;
 import com.example.ticketflow.application.ReservationService;
 import com.example.ticketflow.application.dto.ReservationCreateCommand;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,11 +23,18 @@ public class ReservationApiController {
     }
 
     @PostMapping("/api/reservations/{id}/confirm")
-    public ResponseEntity<String> confirmReservation(@PathVariable Long id) {
+    public ResponseEntity<String> confirmReservation(@PathVariable Long id, @RequestHeader(value = "Idempotency-Key", required = false) String idempotencyKey) {
+
+        if (idempotencyKey == null || idempotencyKey.isBlank()) {
+            return ResponseEntity.badRequest().body("Idempotency-Key 헤더가 필요합니다.");
+        }
+
         try{
-            reservationService.confirmReservation(id);
+            reservationService.confirmReservation(id, idempotencyKey);
             return ResponseEntity.ok("예약 확정 성공.");
-        } catch (IllegalStateException | IllegalArgumentException e) {
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(409).body("중복된 요청입니다.");
+        }catch (IllegalStateException | IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
